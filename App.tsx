cb9129acec0165cf83cbb591f1868fe23b9feae9
Emerson Silva <emerson_jdss@hotmail.com>
2023-04-30 11:28:52 -0300

    test with nativewind

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import React, { FC } from 'react';
-import { View } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 
 // import { Container } from './styles';
 
 const App: FC = () => {
-  return <View />;
+  const backgroundStyle = "bg-neutral-300 dark:bg-slate-900 flex-1 align-items-center justify-center"
+  return (
+    <>
+      <SafeAreaView className={backgroundStyle}>
+        <Text>
+          Emerson
+        </Text>
+      </SafeAreaView>
+    </>
+  )
 }
 
 export default App;
