518acad7ac246b454deafaa83552411bfda97252
Emerson Silva <emerson_jdss@hotmail.com>
2023-04-30 11:30:57 -0300

    change theme for tests

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView, Text } from 'react-native';
 // import { Container } from './styles';
 
 const App: FC = () => {
-  const backgroundStyle = "bg-neutral-300 dark:bg-slate-900 flex-1 align-items-center justify-center"
+  const backgroundStyle = "bg-neutral-300 dark:bg-slate-900 flex-1"
   return (
     <>
       <SafeAreaView className={backgroundStyle}>
