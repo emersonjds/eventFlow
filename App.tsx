5d42b5cfebaa747bffb3f22a5079439c9aa38e0a
Emerson Silva <emerson_jdss@hotmail.com>
2023-04-30 12:54:42 -0300

    config new configs for app

--- App.tsx
+++ App.tsx
@@ -4,12 +4,12 @@ import { SafeAreaView, Text } from 'react-native';
 // import { Container } from './styles';
 
 const App: FC = () => {
-  const backgroundStyle = "bg-neutral-300 dark:bg-slate-900 flex-1"
+  const backgroundStyle = "container h-12 bg-slate-300 flex-1"
   return (
     <>
       <SafeAreaView className={backgroundStyle}>
         <Text>
-          Emerson
+          Start New Project
         </Text>
       </SafeAreaView>
     </>
